2327c71671043b35deb450c65b424de42ef22e7c
Jonesyj83 <jonesyj83@gmail.com>
2021-02-15 14:36:10 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,12 +4,20 @@ if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]
     echo "############################################"
     echo "Uninstalling PiHole Argonone Fan Hat Monitor"
     echo "############################################"
+    defaultpiholedir=/var/www/html/admin
+    read -p "Where is your Pi-Hole Admin folder? (Default = $defaultpiholedir)" piholedir
+    piholedir=${piholedir:=$defaultpiholedir}
+
+    if [ ! -d "$piholedir" ]; then
+        echo "Directory $piholedir does not exist!! Aborting!"
+        exit
+    fi
     echo "Removing Crontab"
-    sudo crontab -l | sudo grep -v '/var/www/html/admin/scripts/pi-hole/fanspeed/fanspeed.sh' | sudo crontab -
+    sudo crontab -l | sudo grep -v "$piholedir/scripts/pi-hole/fanspeed/fanspeed.sh" | sudo crontab -
     echo "Removing Files and config"
-    sudo rm -r /var/www/html/admin/scripts/pi-hole/fanspeed
-    sudo rm /var/www/html/admin/fanspeed.php
-    sudo rm /var/www/html/admin/api_fanspeed.php
+    sudo rm -r "$piholedir/scripts/pi-hole/fanspeed"
+    sudo rm "$piholedir/fanspeed.php"
+    sudo rm "$piholedir/api_fanspeed.php"
     read -p "Do you wish to delete the database file? " dbfile
     if [[ $dbfile == "y" || $dbfile == "Y" || $dbfile == "yes" || $dbfile == "Yes" ]]; then
         sudo rm /etc/pihole/fanspeed.db
@@ -17,7 +25,7 @@ if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]
     sudo sed -i '/FANSPEED_CHART_DAYS/d' /etc/pihole/setupVars.conf
     sudo sed -i '/FANSPEEDDAYS/d' /etc/pihole/setupVars.conf
     echo "Removing all files from PiHole and returning to standard"
-    cd /var/www/html/admin
+    cd "$piholedir"
     sudo git fetch
     sudo git reset --hard HEAD
     echo "Uninstall Complete"
