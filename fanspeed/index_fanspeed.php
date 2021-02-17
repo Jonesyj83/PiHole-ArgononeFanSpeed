84a9d78f2a6b6a64ac7b2ee191199194760354c5
Jonesyj83 <jonesyj83@gmail.com>
2021-02-17 14:14:16 +0800

    Add function to adjust time on Index chart

--- fanspeed/index_fanspeed.php
+++ fanspeed/index_fanspeed.php
@@ -1,12 +1,17 @@
-
 <div class="row">
   <div class="col-md-12">
     <div class="box" id="queries-over-time">
         <div class="box-header with-border">
           <h3 class="box-title">FanSpeed results over last <?php echo htmlspecialchars($fanspeeddays); ?></h3>
         </div>
+    	   <input type="button" id="1h" class="period ui-button" value="1h" />
+    	   <input type="button" id="3h" class="period ui-button" value="3h"/>
+	   <input type="button" id="6h" class="period ui-button" value="6h"/>
+    	   <input type="button" id="9h" class="period ui-button" value="9h"/>
+    	   <input type="button" id="12h" class="period ui-button" value="12h"/>
+    	   <input type="button" id="24h" class="period ui-button" value="24h"/>
         <div style="width: 100%; overflow: auto">
-        <div style="width: 1025px; height: 140">
+        <div style="height: 140">
           <div class="box-body">
           <div class="chart">
           <canvas id="fanspeedChart" width="0" height="140"></canvas>
