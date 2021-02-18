e496b207f53bb885630b02ef3dda4adbf69be38b
Jonesyj83 <jonesyj83@gmail.com>
2021-02-18 18:04:26 +0800

    Delete header_fanspeed.php

--- fanspeed/header_fanspeed.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($setupVars["FANSPEED_CHART_DAYS"]))
-  	{
-  		  $fanspeeddays = $setupVars["FANSPEED_CHART_DAYS"]." Days";
-  	}else {
-  	    $fanspeeddays  = "24 Hours";
-  	}
-?>
