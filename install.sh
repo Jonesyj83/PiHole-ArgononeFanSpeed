086c40a05d2f7e1498dc83a65c536fabca9b24b2
Jonesyj83 <jonesyj83@gmail.com>
2021-02-18 18:43:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,8 +28,8 @@ if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]
         fi
         git clone https://github.com/Jonesyj83/PiHole-ArgononeFanSpeed.git
         echo "Moving files into PiHole directory"
-        sudo cp "Pihole-ArgononeFanSpeed/fanspeed.js" "$piholedir/scripts/js/"
-        sudo cp "PiHole-ArgononeFanSpeed/fanspeedresults.js" "$piholedir/scripts/js/"
+        sudo cp "PiHole-ArgononeFanSpeed/fanspeed/fanspeed.js" "$piholedir/scripts/pihole/js/"
+        sudo cp "PiHole-ArgononeFanSpeed/fanspeed/fanspeedresults.js" "$piholedir/scripts/pihole/js/"
         sudo cp -r "PiHole-ArgononeFanSpeed/fanspeed" "$piholedir/scripts/pi-hole"
         sudo cp "PiHole-ArgononeFanSpeed/fanspeed.php" "$piholedir"
         sudo cp "PiHole-ArgononeFanSpeed/api_fanspeed.php" "$piholedir"
@@ -42,7 +42,7 @@ if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]
         sudo sed -i $'/db_lists.php/{i\t<?php include "./scripts/pi-hole/fanspeed/header_fanspeed.php"; ?>\n:a;n;ba}' "$piholedir/scripts/pi-hole/php/header.php"
         sudo sed -i $'/piholeFTLConfig/{i$dbFanSpeed ="/etc/pihole/fanspeed.db";\n:a;n;ba}' "$piholedir/settings.php"
         sudo sed -i '/Other API functions/a require("api_fanspeed.php");\n' "$piholedir/api.php"
-        sudo sed -i -e '$a input {\ncolor:#556068;\nbackground-color:#32393e;\ncursor:pointer;\noutline:none;\nborder:none;\n}\ninput:hover,\ninput:focus{\ncolor: #fff;\nbackground-color:#22272a;\noutline:none;\nborder:none;\n}' "$pirholedir/style/themes/default-dark.css"
+        sudo sed -i -e '$a input {\ncolor:#556068;\nbackground-color:#32393e;\ncursor:pointer;\noutline:none;\nborder:none;\n}\ninput:hover,\ninput:focus{\ncolor: #fff;\nbackground-color:#22272a;\noutline:none;\nborder:none;\n}' "$piholedir/style/themes/default-dark.css"
         sudo sed -i -e '$a input {\ncolor:#4b646f;\nbackground-color:#fff;\ncursor:pointer;\noutline:none;\nborder:none;\n}\ninput:hover,\ninput:focus{\ncolor: #fff;\nbackground-color:#1e282c;\noutline:none;\nborder:none;\n}' "$piholedir/style/themes/default-light.css"
 
         echo "Adding crontab"
